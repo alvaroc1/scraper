823baf11ad1f9c85682aa7fda9bb60e957ba9488
Alvaro Carrasco <simplepic@gmail.com>
2019-12-04 11:05:31 -0800

    Tweaks

--- build.sbt
+++ build.sbt
@@ -1,3 +1,5 @@
 
-libraryDependencies += "org.scalatest" %% "scalatest" % "3.1.0"
-
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "3.1.0",
+  "org.scalatestplus" %% "scalatestplus-selenium" % "1.0.0-M2"
+)
--- src/main/scala/scraper.scala
+++ src/main/scala/scraper.scala
@@ -1,5 +1,13 @@
+import org.scalatestplus.selenium.WebBrowser
+import org.scalatestplus.selenium.Chrome
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
 
-object scraper extends App {
+object scraper extends App with Chrome {
+  //implicit val webDriver: WebDriver = new HtmlUnitDriver
+
+  go to ("https://glassdor.com")
+
   println("Running scraper")
 }
 
