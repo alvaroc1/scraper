31a1d1c82bd05608dfe8f459d9d1dfb385c5f148
Alvaro Carrasco <simplepic@gmail.com>
2019-12-04 16:50:10 -0800

    Pagination

--- src/main/scala/scraper.scala
+++ src/main/scala/scraper.scala
@@ -2,25 +2,12 @@ import org.scalatestplus.selenium.WebBrowser
 import org.scalatestplus.selenium.Chrome
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.htmlunit.HtmlUnitDriver
-import org.openqa.selenium.Keys
+import org.openqa.selenium.{By, Keys}
 import org.scalatest.time.Span
 import org.scalatest.time.Seconds
 
-object scraper extends App with Chrome {
-
-  implicitlyWait(Span(2, Seconds))
-
-  // if some popup is open, close it
-  def closePopupIfOneIsOpen = {
-    find(".SVGInline modal_closeIcon").foreach {closePopupButton =>
-      click on closePopupButton
-    }
-  }
-
-  // find the link for a page (ex: 1, 2, 3, etc)
-  def pageLink (num: Int) = {
-    xpath(s"//a[./text()=$num]")
-  }
+object scraper extends App with Chrome with Utils {
+  implicitlyWait(Span(3, Seconds))
 
   go to ("https://glassdor.com")
 
@@ -39,31 +26,75 @@ object scraper extends App with Chrome {
   click on textField("sc.keyword")
   enter("vp of engineering")
 
+
   // clear location field
   click on textField("sc.location")
   enter("") // clear
   pressKeys(" ") // force field to register change
   pressKeys(Keys.ENTER.toString)
 
-  Thread.sleep(1000)
+  // set company size filter
+  closePopupIfOneIsOpen
+  click on className("more")
+  click on xpath("(//*[@data-test='SIZE'])[2]") // there's two, we want the second
+  click on xpath("//*[@class='flyout']//*[./text()='1-200 employees']")
 
   closePopupIfOneIsOpen
 
-  // for each job
-  findAll(xpath("//li[@class='jl']")).foreach {element =>
-    println(element.attribute("data-normalize-job-title").get)
+  readJobs
+
+  var page = 2
+  // if we can find a next page, follow it
+  while (find(pageLink(page)).nonEmpty) {
+    click on dealWithOverlay(pageLink(page))
+
+    readJobs
+
+    closePopupIfOneIsOpen
+
+    page = page + 1
   }
-
-  /*
-  click on pageLink(2)
-
-  closePopupIfOneIsOpen
-
-  click on pageLink(3)
-
-  closePopupIfOneIsOpen
-  */
-
-  println("Running scraper")
+  
+  println("DONE")
 }
 
+trait Utils {self: Chrome =>
+
+  // if some popup is open, close it
+  def closePopupIfOneIsOpen = {
+    Thread.sleep(1000) // make sure popup has time to load
+    find(className("modal_closeIcon")).foreach {closePopupButton =>
+      click on closePopupButton
+      Thread.sleep(1000) // allow overlay to clear
+    }
+    find(xpath("//*[@data-test='closeProfileTooltip']")).foreach {closeProfilePopupButton =>
+      click on closeProfilePopupButton
+      Thread.sleep(500)
+    }
+  }
+
+  // find the link for a page (ex: 1, 2, 3, etc)
+  def pageLink (num: Int) = {
+    xpath(s"//a[./text()=$num]")
+  }
+
+  def readJobs = {
+    // for each job
+    findAll(xpath("//li[@class='jl']")).foreach {element =>
+      // query starting from this element
+      // this requires the java-api
+      val companyName = element.underlying.findElement(By.xpath("//*[@class='jobHeader']/a/div")).getText()
+      val jobTitle = element.attribute("data-normalize-job-title").get
+      val location = element.attribute("data-job-loc").get
+
+      println(s"$companyName,$location,$jobTitle")
+    }
+  }
+
+  def dealWithOverlay (query: Query) = {
+    Thread.sleep(1000)
+    closePopupIfOneIsOpen
+    Thread.sleep(1000)
+    query
+  }
+}
